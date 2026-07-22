49d79bc2fc54a469de29c18939877162bc1523d7
Cantibenyam <nyamthenyam@gmail.com>
2026-07-22 00:53:46 -0700

    working directory

---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+export default function aboba({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ app/projects/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h2>You have arrived to my second page!</h2>;
+}
