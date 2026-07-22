48f6342888ee40c11855d647d7da820f17e46866
Cantibenyam <nyamthenyam@gmail.com>
2026-07-22 08:17:48 -0700

    if applied this commit will add a short description under main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Page() {
-  return <h1>Nyam's page</h1>;
+  return (
+    <div>
+      <h1>Nyam's page</h1>
+      <h2> This is my micro - page, it will grow i promise</h2>
+    </div>
+  );
 }
