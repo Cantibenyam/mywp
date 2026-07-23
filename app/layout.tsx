d07cc26825e8dda949ad460026f94d426cf589b5
Cantibenyam <nyamthenyam@gmail.com>
2026-07-22 21:35:47 -0700

    metadata updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import './globals.css';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Nyam's Profile",
+  description: 'my funky profile',
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <title>Nyam's profile</title>
-        <meta name="description" content="This is Nyam's personal profile website"></meta>
-      </head>
       <body>
         <main>{children}</main>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Landing Page',
+  description: 'the main page featuring my profile',
+};
+
 export default function Page() {
   return (
     <div>
