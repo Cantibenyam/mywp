2c66591b14386ccee118b1c693e3a9fac127a8a4
Cantibenyam <nyamthenyam@gmail.com>
2026-07-22 20:10:23 -0700

    font-family changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <div>
       <h1>Nyam's page</h1>
-      <h2> This is my micro - page, it will grow i promise</h2>
+      <p> This is my micro - page, it will grow i promise</p>
     </div>
   );
 }
